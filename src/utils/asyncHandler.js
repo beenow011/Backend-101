855c0babd57cb6ef835cfee2a7ce2fde29dab9fb
beenow011 <abhinavnb11@gmail.com>
2024-01-21 07:52:45 +0530

    utils

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,3 +1,19 @@
-const asyncHandler = () => { }
+const asyncHandler = (reqHandler) => {
+    (req, res, next) => {
+        Promise.resolve(reqHandler(req, res, next)).catch((err) => next(err))
+    }
+}
 
 
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (err) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
+
+export { asyncHandler }
